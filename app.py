9cb55818ce7616ee43b488afca25e4101796f12c
Jorge Weiss <jorge.a.weiss@gmail.com>
2020-07-28 21:52:26 -0500

    Add database URL

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from flask import Flask, request, jsonify, render_template
 from models import db, connect_db, Element
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///elements'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'DATABASE_URL'
+app.config['DATABASE_URL'] = 'postgres://gdxpkgfpsfxfbd:20542176e7530dd981d6d7be61bb84f5ab97080a36b87f9cd8587ce0cbe5b335@ec2-3-215-83-17.compute-1.amazonaws.com:5432/d896frbt90l7ru'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
