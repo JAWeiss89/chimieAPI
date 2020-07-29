8a874730a3948a2a59cc97435b1a472b15cf1aae
Jorge Weiss <jorge.a.weiss@gmail.com>
2020-07-29 13:06:50 -0500

    add os for environ vars

--- app.py
+++ app.py
@@ -2,12 +2,13 @@
 
 from flask import Flask, request, jsonify, render_template
 from models import db, connect_db, Element
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'DATABASE_URL'
-app.config['DATABASE_URL'] = 'postgres://gdxpkgfpsfxfbd:20542176e7530dd981d6d7be61bb84f5ab97080a36b87f9cd8587ce0cbe5b335@ec2-3-215-83-17.compute-1.amazonaws.com:5432/d896frbt90l7ru'
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'mysecretkey512')
 
 connect_db(app)
 
